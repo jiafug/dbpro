216f3878ff0d7571d210e6a5932273ec912f7524
mbauknecht <43986726+mbauknecht@users.noreply.github.com>
2019-07-06 17:32:26 +0200

    Isnt working

--- v2/distanceAverage.py
+++ v2/distanceAverage.py
@@ -9,6 +9,7 @@ import time
 
 DATA_PATH = "C:\\Users\\markb\\Documents\\GitHub\\dbpro2\\v2\\test_seg.csv"
 RESULT_PATH = "C:\\Users\\markb\\Documents\\GitHub\\dbpro2\\v2\\clusterAvg.csv"
+sep = '; '
 
 current_milli_time = lambda: int(round(time.time() * 1000))
     
@@ -28,14 +29,21 @@ def main():
 
         # logging
         #past_time = current_milli_time()
-        print("started... " + str(start_time))
+        print("started...") #+ str(start_time))
         #counter = 0
 
         # read one csv line after another
+        # 'a' for append, 'w' would override the file every time
         with open(RESULT_PATH, 'a') as result_file:
-            result_file.write('cluster, Average Distance, Line Count, lon1, lat1, lon2, lat2')     
+            #result_file.write('/n')
+            #result_file.write('/n')
+            #result_file.write('/n')
+            header = ('cluster' + sep + 'Average Distance' + sep + 'Line Count' + sep + 'lon1' + sep + 'lat1' + sep + 'lon2' + sep + 'lat2')
+            result_file.write(header)     
             print('result_file wrote header')
-            max = 10
+            # max should be small and adjust in runtime
+            # isnt working right now
+            max = 1000000
             avgTotal = 0.0
             countTotal = 0
             for i in range(0, max):
@@ -61,25 +69,31 @@ def main():
                                 print('no number')
                             else:
                                 if isinstance(j, int):
-                                    if int(j) < max:
+                                    if int(j) > max:
                                         max = int(j)
                                     if int(j) == i:
                                         avg += float(row[9])
                                         count += 1  
                                         cluster = j
-                                        lonlat =  (row[1] + ', ' + row[2] + ', ' + row[5] + ', ' + row[6] + ', ')
+                                        lonlat =  (row[1] + sep + row[2] + sep + row[5] + sep + row[6] + sep)
                 if count > 0:
                     avg = avg / count
+                    countTotal += 1
                 avgTotal += avg
-                countTotal = i
-                line = (str(cluster) + ', ' + str(avg) + ', ' + str(count) + ', ' + lonlat)
+                #countTotal = i
+                line = (str(cluster) + sep + str(avg) + sep + str(count) + sep + lonlat + sep + '/n')
                 print(line)
                 result_file.write(line)
+                print('Starting ' + str(i) + '. cluster / ' + str(max) + '.')
             avgTotal = avgTotal / countTotal
             print('avg: ' + str(avgTotal) + '; Count: ' + str(countTotal))
             end_time = current_milli_time()
+            #time = (current_milli_time() - start_time) 
             sek = str(end_time - start_time)
-            print('distanceAverage finished after ' + sek + ' sec.')
+            print("----------write_to_csv(...): BEGIN----------")
+            print('time consumption in s:' + str(sek))
+            print("----------write_to_csv(...): END----------")
+            #print('distanceAverage finished after ' + sek + ' sec.')
     
 
 if __name__ == "__main__":
